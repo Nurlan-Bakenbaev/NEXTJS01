ad10c221607982ccb62785e87456923ce488d49b
Nurlan <iamsamurai1991@gmail.com>
2024-01-12 22:21:11 +0100

    deleted:    .env 	renamed:    src/app/singup/page.tsx -> src/app/signup/page.tsx 	modified:   src/dbConfig/dbConfig.ts

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,9 +1,12 @@
-import mongoose from "mongoose";
-
+import mongoose, { connection } from "mongoose";
 import "dotenv/config";
 export async function connect() {
   try {
-     mongoose.connect(process.env.)
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("MongoDB is connected");
+    });
   } catch (error) {
     console.error("MONGODB is not connected");
     console.log(error);
