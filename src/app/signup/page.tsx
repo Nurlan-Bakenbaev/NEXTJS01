910d7b840d8f28fcf05964d93e7416dc79ad85a6
Nurlan <iamsamurai1991@gmail.com>
2024-01-13 17:50:59 +0100

    modified:   src/app/signup/page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,10 +10,48 @@ const SignUpPage = () => {
     password: "",
     username: "",
   });
-  const onSignup = async () => {
-    
-  };
-  return <div className="text-center w-full">SignUpPage</div>;
+  console.log(user);
+  const onSignup = async () => {};
+  return (
+    <div className="flex flex-col justify-center items-center text-center w-full h-screen">
+      <h1 className="text-2xl mb-3">Signup</h1>
+      <form className="flex flex-col gap-3">
+        <input
+          className="text-black pl-3 py-2  rounded-md outline-none"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          placeholder="Username"
+          type="text"
+        />
+        <input
+          className="text-black pl-3 py-2  rounded-md outline-none"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Email"
+          type="email"
+        />
+        <input
+          className="text-black pl-3 py-2  rounded-md outline-none"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+          type="password"
+        />
+        <button
+          onClick={onSignup}
+          className="hover:bg-slate-700 border p-2 rounded-md text-sm"
+        >
+          Signup
+        </button>
+        <Link
+          className=" hover:text-blue-600 text-blue-400 text-[13px] text-right"
+          href="/login"
+        >
+          Already have an account?
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpPage;
