0507b1fa5dca603cd8686c464ae4273b2c61a2a9
Nurlan <iamsamurai1991@gmail.com>
2024-01-13 16:55:18 +0100

    modified:   src/app/signup/page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { axios } from "axios";
 
 const SignUpPage = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const onSignup = async () => {
+    
+  };
   return <div className="text-center w-full">SignUpPage</div>;
 };
 
