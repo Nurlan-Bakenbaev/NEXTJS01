c2e4048c2f53802d068420811d8ab05cf09dc904
Nurlan <iamsamurai1991@gmail.com>
2024-01-16 09:08:18 +0100

    modified:   src/app/api/users/aboutuser/route.ts

--- src/app/api/users/aboutuser/route.ts
+++ src/app/api/users/aboutuser/route.ts
@@ -1,1 +1,15 @@
-import 
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+connect();
+export async function GET(request: NextRequest) {
+  try {
+    const tokenData = await getDataFromToken(request);
+    const userId = tokenData?.id;
+    const userFound = await User.findById({ _id: userId });
+    return NextResponse.json(userFound, { status: 200 });
+  } catch (error: any) {
+    NextResponse.json({ msg: error.message });
+  }
+}
